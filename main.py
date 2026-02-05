27e06512a8db7a663a776f358c1e1bcf31298368
Rip <ivan.kurilin@gmail.com>
2026-02-05 13:06:23 +0100

    refact

--- main.py
+++ main.py
@@ -36,8 +36,9 @@ class Config:
     CLEANED_JSON_OUTPUT = JSON_OUTPUT_DIR / (PPTX_INPUT.stem + "_cleaned.json")
     
 
-    AGENT_MAX_RETRIES = 3    
-    AGENT_LLM_MODEL = 'qwen3:8b' 
+ 
+    AGENT_LLM_MODEL = 'qwen2.5-coder:7b' 
+    # AGENT_LLM_MODEL = 'qwen3:8b' 
 
     @classmethod
     def setup_directories(cls):
